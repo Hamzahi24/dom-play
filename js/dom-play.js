351415adb65b097ffcacfa560ffefc2a2f7b1319
Hamzah Ibrahim <hamzahibrahim2012@gmail.com>
2023-12-12 13:30:51 -0800

    Added conversations

---
+++ js/dom-play.js
@@ -0,0 +1,21 @@
+function highlightActor(actor){
+    
+    const spans = document.querySelectorAll("div#play span");   
+    for(const mySpan of spans){
+        if(mySpan.dataset.actor == actor){
+            mySpan.style.backgroundColor='green';
+        }else{
+            mySpan.style.backgroundColor='white';
+        }
+    }
+}
+
+const spans = document.querySelectorAll("div#play span");
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click",function(ev){
+
+        highlightActor(mySpan.dataset.actor);
+    });
+}
